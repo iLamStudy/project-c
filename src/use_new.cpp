3068028e77ba95c9ab323de0e3c2b0b63b5fe1a2
lamborcai <471127079@qq.com>
2018-12-03 15:55:12 +0800

    new file:   src/use_new.cpp

---
+++ src/use_new.cpp
@@ -0,0 +1,29 @@
+//use_new.cpp --using the new operator
+#include <iostream>
+
+int main() {
+	using namespace std;
+	
+	int nights = 1001;
+	int *pt = new int;
+	*pt	= 1001;
+
+	cout << "nights values = ";
+	cout << nights << ":location " << &nights << endl;
+
+	cout << "int ";
+	cout << "value = " << *pt << ": location = " << pt << endl;
+	
+	double * pd = new double;//allocate space for a double
+	*pd = 100000001.1;
+
+	cout << "double ";
+	cout << "value = " << *pd << ":location = " << pd << endl;
+
+	cout << "location of poniter pd : " << &pd << endl;
+	cout << "size of *pt :" << sizeof(*pt) << endl;
+	cout << "size of pd :" << sizeof(pd) <<endl; 
+	cout << "size of *pd :" << sizeof(*pd) << endl;
+	return 0;
+
+}
