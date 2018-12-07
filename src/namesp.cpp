256c9eea58134a2f3a34be08e8f0236409ac739a
lambor.cai <lamborcai@gmail.com>
2018-12-07 12:18:36 +0800

    修改：      src/namesp.cpp 	新文件：    src/namessp.cpp

--- src/namesp.cpp
+++ src/namesp.cpp
@@ -22,6 +22,19 @@ namespace debts {
 	void getDebt(Debt & rd) {
 		getPerson(rd.name);
 		std::cout << "Enter debt : ";
-		std::cin 
+		std::cin >> rd.amount; 
+	}
+
+	void showDebt(const Debt & rd){
+		showPerson(rd.name);
+		std::cout << " : $ " << rd.amount << std::endl; 
+	}
+
+	double sumDebts(const Debt ar[], int n) {
+		double total = 0;
+		for(int i = 0;i<n;i++)
+			total += ar[i].amount;
+
+		return total;
 	}
 }
