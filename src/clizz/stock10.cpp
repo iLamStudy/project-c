69fc46e8ff8d7418095c56fa43d8188979bf414a
lanbery.cai <471127079@qq.com>
2018-12-10 10:46:41 +0800

    修改：     src/clizz/stock10.cpp 	新文件：   src/clizz/usestock2.cpp

--- src/clizz/stock10.cpp
+++ src/clizz/stock10.cpp
@@ -11,4 +11,79 @@ Stock::Stock() {//default constructor
 	total_val_ = 0.0;
 }
 
-//TODO
+//
+Stock::Stock(const std::string &company,long n,double price) {
+	std::cout << "Constructor using " << company << " called \n";
+	company_ = company;
+
+	if(n <0) {
+		std::cout << "Number of shares can't be negative; "
+			<< company << " shares set to 0 . \n";
+		shares_ = 0;
+	}else {
+		shares_ = n;
+	}
+
+	share_val_ = price;
+
+	set_total();
+}
+
+//class destructor 
+Stock::~Stock() {
+	std::cout << "Bye, " << company_ << "! \n";
+}
+
+void Stock::buy (long num,double price) {
+	if(num < 0 ) {
+		std::cout << "Number of shares purchased can't be negative. "
+			<< "Transaction is aborted, \n";
+	}else {
+		shares_ += num;
+		share_val_ = price;
+		set_total();
+	}
+}
+
+void Stock::sell(long num,double price) {
+	using std::cout;
+
+	if(num < 0 ) {
+		cout << "Number of shares sold can't be negative. "
+			<< "Transaction is aborted. \n";
+	}else {
+		shares_ -= num;
+		share_val_ = price;
+		set_total();
+	}
+}
+
+void Stock::update(double price) {
+	share_val_ = price;
+	set_total();
+}
+
+void Stock::show() {
+	using std::cout;
+	using std::ios_base;
+
+	//set format to #.###
+	ios_base::fmtflags orig = 
+		cout.setf(ios_base::fixed , ios_base::floatfield);
+
+	std::streamsize prec = cout.precision(3);
+
+	cout << "Company: " << company_ << " Shares : " << shares_ << '\n';
+
+	cout << " Share Price : $" << share_val_;
+
+	//set format to #.##
+	cout.precision(2);
+	cout << " Total Worth: $" << total_val_ << '\n';
+
+	//restore original format
+	cout.setf(orig,ios_base::floatfield);
+	cout.precision(prec);
+}
+
+
