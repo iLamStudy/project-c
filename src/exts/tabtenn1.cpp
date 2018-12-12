56a8589ce6fc12e94f380adc7489bb92de3876df
lanbery.cai <471127079@qq.com>
2018-12-12 18:24:37 +0800

    新文件：   tabtenn1.cpp 	新文件：   tabtenn1.h 	新文件：   usetabtenn1.cpp

---
+++ src/exts/tabtenn1.cpp
@@ -0,0 +1,28 @@
+//tabtenn1.cpp -- simple base-class implements
+#include "tabtenn1.h"
+#include <iostream>
+
+TableTennisPlayer::TableTennisPlayer (
+		const string &fn,const string &ln,bool ht) 
+	: firstname(fn) ,lastname(ln),hasTable(ht) 
+{
+	//firstname 	= fn; 
+	//lastname 	= ln;
+	//hasTable	= ht;
+}
+
+void TableTennisPlayer::Name() const {
+	std::cout << lastname << "," << firstname << " \n";
+}
+
+//RatePlayer methods
+RatedPlayer::RatedPlayer(unsigned int r,const string & fn,
+		const string & ln,bool ht):TableTennisPlayer(fn,ln,ht){
+	rating = r;
+}
+
+RatedPlayer::RatedPlayer(unsigned int r,const TableTennisPlayer & tp) 
+	: TableTennisPlayer(tp),rating(r){
+
+}
+
