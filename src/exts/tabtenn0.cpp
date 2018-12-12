8576e02b3b0d3c3c25904cd3b5ae9a7fbeb96214
lanbery.cai <471127079@qq.com>
2018-12-12 17:51:05 +0800

    新文件：   src/exts/tabtenn0.cpp 	新文件：   src/exts/tabtenn0.h 	新文件：   src/exts/usetnn0.cpp

---
+++ src/exts/tabtenn0.cpp
@@ -0,0 +1,18 @@
+//tabtenn0.cpp -- simple base-class implements
+#include "tabtenn0.h"
+#include <iostream>
+
+TableTennisPlayer::TableTennisPlayer (
+		const string &fn,const string &ln,bool ht) 
+	: firstname(fn) ,lastname(ln),hasTable(ht) 
+{
+	//firstname 	= fn; 
+	//lastname 	= ln;
+	//hasTable	= ht;
+}
+
+void TableTennisPlayer::Name() const {
+	std::cout << lastname << "," << firstname << " \n";
+}
+
+
